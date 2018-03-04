52efe553ea233e4fd6af41c4da8c00bfb0281045
Rodrigo Cabezas Quirós <rcmikado@gmail.com>
2018-03-04 13:09:16 +0100

    asd

---
+++ Prac1/exercici7/build/Debug/Cygwin-Windows/Circle.o.d
@@ -0,0 +1,5 @@
+build/Debug/Cygwin-Windows/Circle.o: Circle.cpp Circle.h Ellipse.h
+
+Circle.h:
+
+Ellipse.h:
---
+++ Prac1/exercici7/build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,8 @@
+build/Debug/Cygwin-Windows/main.o: main.cpp Circle.h Ellipse.h \
+ EllipseContainer.h
+
+Circle.h:
+
+Ellipse.h:
+
+EllipseContainer.h:
